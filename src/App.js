4cd08559792cbce799532b8df98c051174cab56b
Dara Javaherian <darajavaherian@gmail.com>
2018-03-10 06:51:56 +1100

    get working for mobile keyboard maybe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 import QuestionPicker from './components/containers/QuestionPicker/QuestionPicker'
 
 class App extends Component {
+  componentDidMount() {
+  	document.body.addEventListener('click', () => {
+  	  document.getElementById('hidden-field').focus()
+  	})
+  	
+  	document.getElementById('hidden-field').focus()
+
+  }
+
   render() {
     return (
       <div>
