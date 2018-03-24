d7451ae131b59f6cfab05a43854d92e47e885618
Dara Javaherian <darajavaherian@gmail.com>
2018-03-24 01:05:27 +0100

    zh-cn... I have to fix this

--- src/components/containers/QuestionPicker/QuestionPicker.js
+++ src/components/containers/QuestionPicker/QuestionPicker.js
@@ -110,12 +110,12 @@ class QuestionPicker extends Component {
       let progress = this.progress;
 
       let answer = this.state.word.en;
-      let question = this.state.word['es'];
+      let question = this.state.word['zh-cn'];
 
       if (progress[this.state.word.en]) {
         if (progress[this.state.word.en].score >= 1) {
           question = this.state.word.en;
-          answer = this.state.word['es'];
+          answer = this.state.word['zh-cn'];
         }
       }
       
@@ -130,7 +130,7 @@ class QuestionPicker extends Component {
             newQuestion={this.selectQuestion}
             words={this.knownQuestions.length}
             knows={'en'}
-            learning={'es'}
+            learning={'zh-cn'}
             word={this.state.word}
           />
         </div>
