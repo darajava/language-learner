a9e7236e7e5452f37f77beb8b07286dc968c95d5
Dara Javaherian <darajavaherian@gmail.com>
2018-03-26 01:22:49 +0200

    de

--- src/components/containers/QuestionPicker/QuestionPicker.js
+++ src/components/containers/QuestionPicker/QuestionPicker.js
@@ -112,7 +112,7 @@ class QuestionPicker extends Component {
 
 
     render() {
-      let learningLang = 'es';
+      let learningLang = 'de';
       let knowsLang = 'en';
 
       let progress = this.progress;
